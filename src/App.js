5f8f6d100581107c2320041637819f599151bfbc
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2023-02-17 16:36:08 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Routes ,
+  Route,
+  Navigate
+} from "react-router-dom";
+
 
 function App() {
+  const user = true;
+
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Router>
+       <Routes >
+          <Route path="/" element={<Home/>} />
+          <Route path="/products/:category" element={<ProductList/>} />
+          <Route path="/product/:id" element={<Product/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={user ? <Navigate to="/" replace />  : <Login/>} />
+          <Route path="/register" element={user ? <Navigate to="/" replace /> : <Register/>} />
+        </Routes >
+    </Router>
   );
 }
 
