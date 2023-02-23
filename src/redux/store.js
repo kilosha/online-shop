3b4e289a028e45383be818bc9c0da1ef8114d6a8
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2023-02-23 15:13:43 +0300

    add redux persist for saving store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 import cartReducer from './cartSlice';
 import userReducer from './userSlice';
 
-export default configureStore({
-    reducer: {
-        cart: cartReducer,
-        user: userReducer
-    }
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    user: userReducer
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store =  configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
